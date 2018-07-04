7c68b20696d5da464cbd9119b14de9d7f65a70bb
Ben McAlindin <bmcalindin@gmail.com>
2018-07-04 14:48:17 +0100

    Reduce expanded size and increase animation speed

--- src/components/Navbar/constants.styles.js
+++ src/components/Navbar/constants.styles.js
@@ -1,11 +1,11 @@
 import { css } from 'styled-components';
 
 export const collapsedWidth = 70;
-export const expandedWidth = 200;
+export const expandedWidth = 160;
 export const width = expanded => expanded ? expandedWidth : collapsedWidth;
 export const height = 100;
 
-const animationSpeed = '0.3s';
+const animationSpeed = '0.25s';
 export const widthAnimation = css`
   -webkit-transition: width ${animationSpeed} ease-in-out;
   -moz-transition: width ${animationSpeed} ease-in-out;
