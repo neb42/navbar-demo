18ad2406f3c5f273aea982049c3c01061e6fe2b7
Ben McAlindin <bmcalindin@gmail.com>
2018-07-17 13:50:21 +0100

    Make footer and content width 100%

--- src/components/Navbar/NavbarContent/NavbarContent.styles.js
+++ src/components/Navbar/NavbarContent/NavbarContent.styles.js
@@ -5,4 +5,5 @@ export const NavbarContent = styled.div`
   background-color: ${PALETTE.black};
   padding: 5px 0;
   margin: 10px 0;
+  width: 100%;
 `;
--- src/components/Navbar/NavbarFooter/NavbarFooter.styles.js
+++ src/components/Navbar/NavbarFooter/NavbarFooter.styles.js
@@ -4,4 +4,5 @@ import { PALETTE } from '@asidatascience/adler-ui';
 export const NavbarFooter = styled.div`
   position: absolute;
   bottom: 15px;
+  width: 100%;
 `;
